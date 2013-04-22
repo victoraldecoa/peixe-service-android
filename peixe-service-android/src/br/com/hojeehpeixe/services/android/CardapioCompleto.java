d837d0797b51fea9c08601de5c3498a54213cb5a
Victor Aldecoa <victor.maia.a@gmail.com>
2013-04-23 00:32:05 +0200

    Adicionado CardapioCompleto

---
+++ peixe-service-android/src/br/com/hojeehpeixe/services/android/CardapioCompleto.java
@@ -0,0 +1,11 @@
+package br.com.hojeehpeixe.services.android;
+
+public class CardapioCompleto {
+	public Cardapio central;
+	public Cardapio quimica;
+	public Cardapio fisica;
+	public Cardapio prefeitura;
+	public boolean semConexao;
+	public String mensagem;
+	public boolean ehCacheAtual;
+}
